69f8e0f8aa5d45b6bc7cc7c280f9bddbbae5afab
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-03 12:46:44 +0300

    New commit test

---
+++ src/Task_1_2_1.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+
+public class Task_1_2_1 {
+
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+
+        int x=scn.nextInt();
+        int y=scn.nextInt();
+        int z=x+y;
+        if (180>z) {
+            System.out.println("Данный треугольник существует");
+           if (z==90){
+                System.out.println("Данный треугольник прямоугольный");
+            }
+            else {
+                System.out.println("Треугольник не прямоугольный");
+            }
+
+
+        }
+        else {
+            System.out.println("Данный треугольник не существует");
+        }
+    }
+}
