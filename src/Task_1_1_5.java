573ba54071e28a06337c3f72ca9bbb99fd27d852
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-06 20:42:33 +0300

    New commit test

---
+++ src/Task_1_1_5.java
@@ -0,0 +1,20 @@
+public class Task_1_1_5 {
+    public static void main(String[] args) {
+
+        int t = 101245;
+
+        int h = t/3600;
+
+        int m=(t-(h*3600))/60;
+
+        int s=(t-(h*3600+m*60));
+
+
+
+
+        System.out.println("HH:"+h+" MM:"+m+" SS:"+s);
+
+
+
+    }
+}
