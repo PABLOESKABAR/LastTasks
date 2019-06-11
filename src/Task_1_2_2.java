803145bcbbb9f4f7b40c92cfc3b58c1ce9cf0ced
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-11 12:11:09 +0300

    New commit test

---
+++ src/Task_1_2_2.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+
+public class Task_1_2_2 {
+
+
+    public static void main(String[] args) {
+        int min1,min2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ведите переменную а");
+        int a = scanner.nextInt();
+        System.out.println("Ведите переменную b");
+        int b = scanner.nextInt();
+        System.out.println("Ведите переменную c");
+        int c = scanner.nextInt();
+        System.out.println("Ведите переменную d");
+        int d = scanner.nextInt();
+        if (a<b){min1=a;}
+        else {min1=b;}
+        if (c<d){min2=c;}
+        else {min2=d;}
+        if (min1<min2){
+            System.out.println(min2);
+        }
+        else {
+            System.out.println(min1);
+        }
+
+    }
+}
+
