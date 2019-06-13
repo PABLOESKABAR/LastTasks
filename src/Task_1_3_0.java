a9f3c7c4d2655ab4158ad3b7c57d6d0c219128da
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-13 13:33:25 +0300

    New commit test

--- src/Task_1_3_0.java
+++ src/Task_1_3_0.java
@@ -5,12 +5,14 @@ public class Task_1_3_0 {
 
     public static void main(String[] args) {
         Scanner scaner = new Scanner(System.in);
+        System.out.println("Введите положительное целое число");
         int x = scaner.nextInt();
         int y=0;
-        for (int i = 0; i <x+1; i++) {
+        for (int i = 0; i <=x; i++) {
             y = y+i;
-            System.out.println(y);
+
         }
+        System.out.println("Сумма чисел от 0 до "+x+" равна "+y);
     }
 
 }
