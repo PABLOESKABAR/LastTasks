691cac44327f4dd430a8c3ce14d395fbfd10c57e
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-16 21:48:05 +0300

    Произведение первых 200 квадратов чисел

---
+++ src/Task_1_3_3.java
@@ -0,0 +1,15 @@
+
+
+public class Task_1_3_3 {
+
+
+    public static void main(String[] args) {
+      int x=0;
+      int y;
+        for (int i = 0 ; i <101 ; i++) {
+            y = i*i;
+            x = x+y;
+        }
+        System.out.println("Сумма квадратов первых 100 чисел равна - "+x);
+    }
+}
