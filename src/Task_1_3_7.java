49b8e5625b3e9d0b979b6b0a2f77333e528679b1
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-06-16 23:43:52 +0300

    Найти делители в числовом диапозоне от m до n.

---
+++ src/Task_1_3_7.java
@@ -0,0 +1,54 @@
+
+
+import java.util.Scanner;
+
+public class Task_1_3_7 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Введите переменную m");
+        int m = scn.nextInt();
+        System.out.println("Введите переменную n");
+        int n = scn.nextInt();
+        for (int i = m; i <=n; i++) {
+            System.out.print("Число "+i+" имеет следующие делители: ");
+            if (i%2==0){
+                System.out.print(2+" ");
+
+            if (i%3==0){
+                System.out.print(3+" ");
+
+            if (i%4==0){
+                System.out.print(4+" ");
+
+            if (i%5==0){
+                System.out.print(5+" ");
+
+            if (i%6==0){
+                System.out.print(6+" ");
+
+            if (i%7==0){
+                System.out.print(7+" ");
+
+            if (i%8==0){
+                System.out.print(8+" ");
+
+            if (i%9==0){
+                System.out.print(9+" ");
+            }
+            }
+            }
+            }
+            }
+            }}}
+            else {
+                System.out.print("Делится либо на один либо на само себя");
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+
