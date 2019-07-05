feacec69efb673ef6f29e06b70c70f1388184c77
PABLOESKABAR <37873863+PABLOESKABAR@users.noreply.github.com>
2019-07-05 23:17:32 +0300

    New task

---
+++ src/_2_Algoritmization/Task_2_1_7.java
@@ -0,0 +1,16 @@
+package _2_Algoritmization;
+
+public class Task_2_1_7 {
+    public static void main(String[] args) {
+        int[] arr = new int[30];
+        System.out.print("Строка до сортировки    ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) Math.round((Math.random() * 30));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println(" ");
+        for (int i = 0; i <arr.length ; i++) {
+
+        }
+    }
+}
